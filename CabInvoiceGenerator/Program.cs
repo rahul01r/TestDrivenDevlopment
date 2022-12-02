9d7549c9381cb3043a10b9851c0e79a42ebd9adc
rahul01r <rrdubeynibm@gmail.com>
2022-12-02 23:11:57 +0530

    [rahul] Add:UC1-CabInvoiceGenerator

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Cab Service");
+            //UC1
+            CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+            double fare = cabInvoiceGenerator.CalculatFare(10, 5);
+            Console.WriteLine("Cab Fare=" + fare);
+
+        }
+    }
+}
---
+++ CabInvoiceGenerator/Ride.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class Ride
+    {
+        public double rideDistance;
+        public int rideTime;
+        public Ride(double rideDistance, int rideTime)
+        {
+            this.rideDistance = rideDistance;
+            this.rideTime = rideTime;
+        }
+    }
+}
